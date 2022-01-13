4c08ccf9fbf29e9e8cd20490a89ee788693d2319
suddiyo <suddii01@sju.ac.kr>
2022-01-14 00:00:54 +0900

    2022_01_13

--- baekjoon/2022_01_13/2164.cpp
+++ baekjoon/2022_01_13/2164.cpp
@@ -5,19 +5,19 @@ int main(){
     queue<int> Q;
     int N;
     scanf("%d", &N);
+
     for(int i=1;i<=N;i++){
         Q.push(i);
     }
-
-    for(int i=0;i<N;i++){
-
-        if(i%2==0){
+    int i=0;
+    while(Q.size() > 1) {
+        if (i % 2 == 0) {
             Q.pop();
-        }
-        else{
+        } else {
             Q.push(Q.front());
             Q.pop();
         }
+        i++;
     }
     printf("%d\n", Q.front());
     return 0;
