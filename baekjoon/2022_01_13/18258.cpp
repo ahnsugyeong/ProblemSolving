9831b1032f0fe8cf9e9abc8b4e524331e8465f91
suddiyo <suddii01@sju.ac.kr>
2022-01-14 00:19:36 +0900

    2022_01_13

--- baekjoon/2022_01_13/18258.cpp
+++ baekjoon/2022_01_13/18258.cpp
@@ -2,37 +2,39 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false);
     cin.tie(0);
+
     queue<int> Q;
     int N;
-    scanf("%d", &N);
-    getchar();
+    cin >> N;
+    cin.get();
     for(int i=0;i<N;i++){
         string order;
         cin >> order;
-        getchar();
+        cin.get();
         if(order.compare("push")==0){
             int X;
-            scanf("%d", &X);
+            cin >> X;
             Q.push(X);
         } else if(order.compare("pop")==0){
-            if(Q.empty()) printf("-1\n");
+            if(Q.empty()) cout << -1 << "\n";
             else {
-                printf("%d\n", Q.front());
+                cout << Q.front() << "\n";
                 Q.pop();
             }
         } else if(order.compare("size")==0){
-            printf("%d\n", Q.size());
+            cout << Q.size() << "\n";
         } else if(order.compare("empty")==0){
-            printf("%d\n", Q.empty());
+            cout << Q.empty() << "\n";
 
         } else if(order.compare("front")==0){
-            if(Q.empty()) printf("-1\n");
-            else printf("%d\n", Q.front());
+            if(Q.empty()) cout << -1 << "\n";
+            else cout << Q.front() << "\n";
 
         } else if(order.compare("back")==0){
-            if(Q.empty()) printf("-1\n");
-            else printf("%d\n", Q.back());
+            if(Q.empty()) cout << -1 << "\n";
+            else cout << Q.back() << "\n";
         }
     }
     return 0;
