55ea6c348bfde17638acc4571a6a3310104ee5b1
suddiyo <suddii01@sju.ac.kr>
2022-02-22 01:35:52 +0900

    2022_02_21

--- cmake-build-debug/CMakeFiles/Problem_Solving.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Problem_Solving.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Problem_Solving.dir/baekjoon/2022-february/2022_02_20/19939.cpp.o"
+  "CMakeFiles/Problem_Solving.dir/baekjoon/2022-february/2022_02_21/2822.cpp.o"
   "Problem_Solving"
   "Problem_Solving.pdb"
 )
