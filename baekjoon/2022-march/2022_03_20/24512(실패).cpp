13e05ee708d3d8cf82fb952812d5a3e566ec74de
suddiyo <suddii01@sju.ac.kr>
2022-04-02 23:55:15 +0900

    2022_04_02

--- baekjoon/2022-march/2022_03_20/24512(실패).cpp
+++ baekjoon/2022-march/2022_03_20/24512(실패).cpp
@@ -9,23 +9,23 @@ int minAns = INT_MAX;
 int N, M;
 vector<int> V;
 
-void DFS(int index, int cost){
+void DFS(int index, int cost) {
     vertex_visited[index] = true;
 
     int flag = 0;
-    for(int i=1;i<=N;i++){
-        if(!vertex_visited[i]) flag = 1;
+    for (int i = 1; i <= N; i++) {
+        if (!vertex_visited[i]) flag = 1;
     }
-    if(flag == 0) {
+    if (flag == 0) {
         printf("cost = %d\n", cost);
         vertex_visited[index] = false;
         minAns = min(minAns, cost);
         return;
     }
 
-    for(int i=1;i<=N;i++){
-        if(G[index][i] != 0){
-            if(edge_visited[index][i] || vertex_visited[i]) continue;
+    for (int i = 1; i <= N; i++) {
+        if (G[index][i] != 0) {
+            if (edge_visited[index][i] || vertex_visited[i]) continue;
             edge_visited[index][i] = true;
             DFS(i, cost + G[index][i]);
             edge_visited[index][i] = false;
@@ -33,16 +33,16 @@ void DFS(int index, int cost){
     }
 }
 
-int main(){
+int main() {
 
     scanf("%d %d", &N, &M);
 
-    for(int i=0;i<M;i++){
+    for (int i = 0; i < M; i++) {
         int u, v, c;
         scanf("%d %d %d", &u, &v, &c);
         G[u][v] = c;
     }
-    for(int i=1;i<=N;i++){
+    for (int i = 1; i <= N; i++) {
         DFS(i, 0);
         printf("///////////\n");
     }
