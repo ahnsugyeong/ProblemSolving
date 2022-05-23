5cc6faac7f63fc17a41e26703b6347bd3050b62b
suddiyo <suddii01@sju.ac.kr>
2022-05-23 23:58:51 +0900

    2022_05_23

--- baekjoon/2022-may/2022_05_23/2193.cpp
+++ baekjoon/2022-may/2022_05_23/2193.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-int dp[91];
+long long dp[91];
 
 int main() {
     dp[1] = 1;
     dp[2] = 1;
-    int N;
-    scanf("%d", &N);
+    long long N;
+    cin >> N;
     for (int i = 3; i <= N; i++) dp[i] = dp[i - 1] + dp[i - 2];
 
-    printf("%d", dp[N]);
+    cout << dp[N];
 
     return 0;
 }
@@ -19,19 +19,27 @@ int main() {
  * N = 1
  * 1
  * 1
+ *
  * N = 2
  * 10
  * 1
+ *
  * N = 3
  * 101
  * 100
  * 2
+ *
  * N = 4
  * 1000
  * 1001
  * 1010
  * 3
+ *
  * N = 5
  * 10000
  * 10001
+ * 10010
+ * 10100
+ * 10101
+ * 5
  */
