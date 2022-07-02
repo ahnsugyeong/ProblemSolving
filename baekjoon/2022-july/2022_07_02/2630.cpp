5ddb5904fb97e736c3f3900355ae80bb75d3729b
suddiyo <suddii01@sju.ac.kr>
2022-07-03 01:04:19 +0900

    2022_07_02

--- baekjoon/2022-july/2022_07_02/2630.cpp
+++ baekjoon/2022-july/2022_07_02/2630.cpp
@@ -3,16 +3,40 @@
 using namespace std;
 
 int N;
-int paper_1[128][128];
-int paper_2[64][64];
-int paper_3[32][32];
-int paper_4[16][16];
-int paper_5[8][8];
-int paper_6[4][4];
-int paper_7[2][2];
+int w = 0, b = 0;
+int paper[128][128];
+
+void check(int start_i, int start_j, int size) {
+
+    int cnt = 0;
+    for (int i = start_i; i < start_i + size; i++) {
+        for (int j = start_j; j < start_j + size; j++) {
+            if (paper[i][j] == 1) cnt++;
+        }
+    }
+
+    if (cnt == 0) w++;
+    else if (cnt == size * size) b++;
+    else {
+        check(start_i, start_j, size / 2);
+        check(start_i, start_j + size / 2, size / 2);
+        check(start_i + size / 2, start_j, size / 2);
+        check(start_i + size / 2, start_j + size / 2, size / 2);
+    }
+}
+
 
 // 0: 하얀색, 1: 파란색
-int main(){
-    
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> paper[i][j];
+        }
+    }
+    check(0, 0, N);
+
+    cout << w << "\n" << b;
+
     return 0;
 }
--- cmake-build-debug/CMakeFiles/Problem_Solving.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Problem_Solving.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/ahnsugyeong/Desktop/2022/2022-1/Problem_Solving/baekjoon/2022-july/2022_07_02/10974.cpp" "/Users/ahnsugyeong/Desktop/2022/2022-1/Problem_Solving/cmake-build-debug/CMakeFiles/Problem_Solving.dir/baekjoon/2022-july/2022_07_02/10974.cpp.o"
+  "/Users/ahnsugyeong/Desktop/2022/2022-1/Problem_Solving/baekjoon/2022-july/2022_07_02/2630.cpp" "/Users/ahnsugyeong/Desktop/2022/2022-1/Problem_Solving/cmake-build-debug/CMakeFiles/Problem_Solving.dir/baekjoon/2022-july/2022_07_02/2630.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
