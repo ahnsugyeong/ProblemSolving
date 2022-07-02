1af12c0509f91262f92df1919d78706cf0d3f3ef
suddiyo <suddii01@sju.ac.kr>
2022-07-02 23:56:48 +0900

    2022_07_02

---
+++ baekjoon/2022-july/2022_07_02/2630.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+int paper_1[128][128];
+int paper_2[64][64];
+int paper_3[32][32];
+int paper_4[16][16];
+int paper_5[8][8];
+int paper_6[4][4];
+int paper_7[2][2];
+
+// 0: 하얀색, 1: 파란색
+int main(){
+    
+    return 0;
+}
